dd86d17a0d819860735ca3a64c06350767e4f81e
Siddharth-here <siddharthmohapatra2004@gmail.com>
2026-09-03 19:29:55 +0530

    mounting booking routes

--- src/routes/booking.routes.js
+++ src/routes/booking.routes.js
@@ -1,1 +1,12 @@
-import { Router } from "express";
+import { Router, Router } from "express";
+
+import { bookSeats, getMyBookings } from "../controllers/booking.controller.js";
+
+import { authenticate } from "../middleware/auth.middleware.js";
+
+const router = Router()
+
+router.post('/book', authenticate, bookSeats)
+router.get('/my-bookings', authenticate, getMyBookings)
+
+export default router
