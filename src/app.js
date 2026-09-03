8121b055d78dc7279d26282abff6a16d6aa3a5c4
Siddharth-here <siddharthmohapatra2004@gmail.com>
2026-09-03 19:34:34 +0530

    mount the routres in app

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+import authRoutes from "./routes/auth.routes.js";
+import bookingRoutes from "./routes/booking.routes.js";
+
+const app = express();
+app.use(express.json());
+
+app.use("api/auth", authRoutes);
+app.use("/api/bookings", bookingRoutes);
+
+export default app;
